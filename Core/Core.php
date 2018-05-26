f66888d2ac3bfdc345cf9a0a140599da6a4564e2
Chris  Koivu <cmkoivu@comcast.net>
2018-05-26 14:53:43 +0000

    Core.php edited online set default route to home / index

--- Core/Core.php
+++ Core/Core.php
@@ -21,8 +21,8 @@ OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 ////////////////////////////////////////////////////////////////////////////////////////////////////////// */
  
-$controller = null;
-$action = null;
+$controller = 'home';
+$action = 'index';
 $query = null;
 $key = null;
 ini_set ('display_errors', DEBUG);
