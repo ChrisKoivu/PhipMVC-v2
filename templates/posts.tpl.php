13fbb3c96d9bfaca34eedc77a2e233f5144b8d09
Chris  Koivu <cmkoivu@comcast.net>
2018-06-18 15:50:18 +0000

    posts.tpl.php cleaned up

--- templates/posts.tpl.php
+++ templates/posts.tpl.php
@@ -3,19 +3,17 @@
 
 <div class="content container">  
 
-    <?php 
-    print_r($posts);
-    print sizeof ($posts);
-    if (!empty($posts))  { ?>
-        <div class="post">
-          <div class="col-md-12  ">
-            <h3><?php echo $posts['title']; ?></h3>
-            <p><?php echo $posts['body'];?></p>
-            <p><a class="btn btn-secondary" href="<?php echo $post_value['post_link']; ?>" role="button">View details &raquo;</a></p>
-            <hr>          
-          </div>     
-        </div><!-- end of post section -->
-      
+    <?php if (!empty($posts))  { ?>
+       <?php foreach($posts as $post) {?>
+          <div class="post">
+            <div class="col-md-12  ">
+               <h3><?php echo $post['title']; ?></h3>
+               <p><?php echo $post['body'];?></p>
+               <p><a class="btn btn-secondary" href="<?php echo $post['post_link']; ?>" role="button">View details &raquo;</a></p>
+               <hr>          
+            </div>     
+          </div><!-- end of post section -->
+        <?php } ?>
     <?php  } else {   
        echo 'there are no posts yet';  
     }
