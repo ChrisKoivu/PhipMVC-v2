6c8af450c3c54122be4d4ae40300b468c8976534
Chris  Koivu <cmkoivu@comcast.net>
2018-06-20 18:19:12 +0000

    error.tpl.php created online with Bitbucket

---
+++ templates/error.tpl.php
@@ -0,0 +1,8 @@
+ <!-- Main jumbotron for error output -->
+      <div class="jumbotron">
+        <div class="container">
+          <h1 class="display-3">Error !</h1>
+          <p><?php echo $error; ?></p>
+          <p><a class="btn btn-primary btn-lg" href="#" role="button">Close &raquo;</a></p>
+        </div>
+      </div>
