515a99d15be0c7a6e264af9c8b040d4d9f940cc6
chriskoivu <cmkoivu@comcast.net>
2018-06-30 08:48:51 -0400

    clean up of nonce class

--- Core/includes/nonce.inc.php
+++ Core/includes/nonce.inc.php
@@ -1,21 +1,18 @@
 <?php
 
-require_once 'Nonce.php';
+    require_once 'Nonce.php';
+    $nonce = New Nonce ();
+    $cnonce = $nonce->get_nonce();
+    $hash = hash('sha512', $cnonce);
 
-$nonce = New Nonce ( );
+    // check if valid nonce on form submission
+    if(isset($_POST["submit"])) {
+        If($nonce->verify_nonce (cnonce, $hash)){
 
-$cnonce = $nonce->get_nonce( );
-$hash = hash('sha512', $cnonce);
+        } else {
+        Print 'Invalid Form Submission';
 
-if(isset($_POST["submit"])) {
-
-If($nonce->verify_nonce (cnonce, $hash)){
-
-} else {
-Print 'Invalid Form Submission';
-
-}
-
-}
+        }
+    }
 
 ?>
--- Libraries/Nonce.php
+++ Libraries/Nonce.php
@@ -1,53 +1,57 @@
 <?php
 
 Class Nonce {
-Private $nonce_id = NULL;
-Private $server_nonce = NULL;
+    Private $nonce_id = NULL;
 
-Public function __construct( ){
+    Public function __construct( ){
+    
+    } // end of constructor
+
+    // save generated nonce to the session
+    Private function store_nonce ($nonce_id, $nonce) {
+        $server_nonce = array (
+            'nonce_id'=> $nonce_id,
+            'nonce'=> $nonce 
+        );
+        $session = New Session();
+        set_session_value($nonce_id, $server_nonce);
+    } // end of store nonce
  
-}
+    // generate a new nonce for the form
+    Public function get_nonce ( ) {
+        $token = New Token( );
+        $this->nonce_id = $token->generate_token (16);
+        $nonce = hash('sha512', $token->generate_token (256));
+        $this->store_nonce ($this->nonce_id, $nonce);
+        return $nonce;
+    } // end of get nonce function
 
-Private function store_nonce ($nonce_id, $nonce) {
-  $server_nonce = array (
-    'nonce_id'=> $nonce_id,
-    'nonce'=> $nonce 
-  );
-  $session = New Session();
-  set_session_value($nonce_id, $server_nonce);
-}
+    // retrieve the saved nonce from the session
+    Public function get_stored_nonce ($nonce_id){
+        $session = New Session();
+        return $session->get_session_value($nonce_id);
+    }
 
-Public function get_nonce ( ) {
-$token = New Token( );
-$this->nonce_id = $token->generate_token (16);
-$nonce = hash('sha512', $token->generate_token (256));
-$this->store_nonce ($this->nonce_id, $nonce);
-return $nonce;
-}
+    // verify form nonce is valid
+    Public function verify_nonce($cnonce, $hash) {
+      $nonce = $this->get_stored_nonce($this->nonce_id); // Fetch the nonce from the last request
+      // verify the stored nonce exists
+      if (!empty($nonce)) {
+         remove_nonce($this->nonce_id); 
+         $testHash = hash('sha512', $nonce . $cnonce);
+         // returns the result of the hash comparison
+         return ($testHash === $hash);
+      } else {
+         // there is no nonce, so return false
+         return false;
+      }
+    } // end of verify nonce function
 
-Public function get_stored_nonce ($nonce_id){
-   $session = New Session();
-   return $session->get_session_value($nonce_id);
-}
+    // remove the stored nonce from the session
+    Public function remove_nonce ($nonce_id){
+        $session = New Session();
+        set_session_value($nonce_id, NULL);
+    } // end of remove nonce function
 
-Public function verify_nonce($cnonce, $hash) {
-$nonce = $this->get_stored_nonce($this->nonce_id); // Fetch the nonce from the last request
-if (!empty($nonce)) {
-remove_nonce($this->nonce_id);
-$testHash = hash('sha512', $nonce . $cnonce);
-return $testHash == $hash;
-} else {
-return false;
-}
-}
-
-Public function remove_nonce ($nonce_id){
-$this->server_nonce[$nonce_id]['nonce_id'] = NULL;
-}
-
-
-
-
-
-}
+} // end of nonce class
 ?>
